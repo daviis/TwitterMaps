c3b70531211b681a6f549fa8ca5974808b281da5
daviis <daviis@luther.edu>
2014-03-11 13:39:14 -0500

    found out how to make a pin on the map

--- googleMapsTout.js
+++ googleMapsTout.js
@@ -5,5 +5,11 @@ function initialize() {
    };
    var map = new google.maps.Map(document.getElementById("map-canvas"),
    mapOptions);
+
+   var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(-33, 150),
+      map: map,
+      title: 'Hello World!'
+   })
 }
 google.maps.event.addDomListener(window, 'load', initialize);

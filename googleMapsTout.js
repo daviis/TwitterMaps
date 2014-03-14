7497dee44bd70b41484be0a098ed14455e9cb973
daviis <daviis@luther.edu>
2014-03-13 19:48:15 -0500

    Fixed everthing that I broke in the merge

--- googleMapsTout.js
+++ googleMapsTout.js
@@ -1,11 +1,11 @@
 var map;
-google.maps.event.addDomListener(window, 'load', initialize);
+
 
 function initialize() {
-   var mapOptions = {
+   var mapOptions = { 
    center: findUserLocation(),
    zoom: 8
-   };
+   };  
    map = new google.maps.Map(document.getElementById("map-canvas"),
    mapOptions);
 
@@ -13,10 +13,11 @@ function initialize() {
       position: new google.maps.LatLng(-33, 150),
       map: map,
       title: 'Hello World!'
-   })
+   })  
 }
+google.maps.event.addDomListener(window, 'load', initialize);
 
-var addMarkers = funciton(anArray){
+var addMarkers = function(anArray) {
     for (i=0;i<anArray.length;i++) {
         var myLatlng = new google.maps.LatLng(latList[i].lat,latList[i].lon);
         var marker = new google.maps.Marker({
@@ -24,10 +25,10 @@ var addMarkers = funciton(anArray){
             title:"Hello World!"
         });
         marker.setMap(map);
-    }
+    }   
 }
 //addMarkers()
-setTimeout(function() {addMarkers();},1250);
+//setTimeout(function() {addMarkers();},1250);
 
 
 var findUserLocation = function(){
@@ -57,3 +58,4 @@ var findUserLocation = function(){
     return initialLocation;
   }
 }
+

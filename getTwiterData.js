2fa868bb3ae82d02fa5dec83e991da65e7d91d51
daviis <daviis@luther.edu>
2014-03-15 11:15:40 -0500

    Added submit field and js for calling cgi-bin TwitterAPI calls

---
+++ getTwiterData.js
@@ -0,0 +1,17 @@
+var requestNewData = function(){
+	var searchTerm = document.getElementById("searChTerm").value
+	var request = new XMLHttpRequest()
+	var url = "http://localhost:8000/cgi-bin/TwitterMapsCGI.py?searchTerm="+searchTerm
+	
+	request.onreadystatechange = function(){
+		if(request.readyState == 4 && request.status == 200)
+		{
+			var responce = JSON.parse(request.responseText)
+			
+			addMarkers(responce)
+		}
+	}	
+
+	request.open("GET", URL, true)
+	request.send()
+}
